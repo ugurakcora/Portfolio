8ab8f70dd26fdae68a907035e0cd9be72c6774c8
ugurakcora <uakcora97@gmail.com>
2021-10-29 01:28:49 +0300

    navbar js added

---
+++ public/app.js
@@ -0,0 +1,10 @@
+// links
+
+const links = document.querySelectorAll('.link');
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        links.forEach(ele => ele.classList.remove('active'));
+        link.classList.add('active');
+    })
+})
