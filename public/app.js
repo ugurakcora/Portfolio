a13b8059eccafbea0d8cbc43f79c659208785284
ugurakcora <uakcora97@gmail.com>
2021-10-29 04:08:06 +0300

    project section finished

--- public/app.js
+++ public/app.js
@@ -8,3 +8,42 @@ links.forEach(link => {
         link.classList.add('active');
     })
 })
+
+
+
+// creating dynamic project card
+
+const projectContainer = document.querySelector('.project-container');
+
+projects.forEach(project => {
+    projectContainer.innerHTML += `
+    <div class="project-card" data-tags="#all, ${project.tags}">
+    <img src="${project.image}" alt="">
+    <div class="content">
+      <h1 class="project-name">${project.name}</h1>
+      <span class="tags">${project.tags}</span>
+    </div>
+  </div>
+    `;
+})
+
+
+// filters
+
+const filters = document.querySelectorAll('.filter-btn');
+
+filters.forEach(filterBtn => {
+    filterBtn.addEventListener('click', () => {
+        let id = filterBtn.getAttribute('id');
+        let projectCards = document.querySelectorAll('.project-card');
+        projectCards.forEach(card => {
+            if(card.getAttribute('data-tags').includes(id)){
+                card.classList.remove('hide');
+            } else{
+                card.classList.add('hide');
+            }
+        })
+        filters.forEach(btn => btn.classList.remove('active'));
+        filterBtn.classList.add('active');
+    })
+})
---
+++ public/project.js
@@ -0,0 +1,32 @@
+let projects = [
+    {
+        name: "project one",
+        tags: "#javascript, #fullstack, #ui, #backend",
+        image: "https://picsum.photos/200",
+    },
+    {
+        name: "project two",
+        tags: "#javascript, #fullstack #ui",
+        image: "https://picsum.photos/200",
+    },
+    {
+        name: "project tree",
+        tags: "#javascript, #fullstack, #backend",
+        image: "https://picsum.photos/200",
+    },
+    {
+        name: "project four",
+        tags: "#javascript, #ui, #backend",
+        image: "https://picsum.photos/200",
+    },
+    {
+        name: "project five",
+        tags: "#fullstack #ui, #backend",
+        image: "https://picsum.photos/200",
+    },
+    {
+        name: "project six",
+        tags: "#javascript, #fullstack, #ui, #backend",
+        image: "https://picsum.photos/200",
+    },
+]
